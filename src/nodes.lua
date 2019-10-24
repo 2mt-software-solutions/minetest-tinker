fc1eb61d7e3a97f12702379afd593c9e738cb449
Jan Schwarz <jan.schwarz@2mt.info>
2019-10-24 16:29:51 +0200

    Add Minevium Ore

--- src/nodes.lua
+++ src/nodes.lua
@@ -34,3 +34,22 @@ minetest.register_node("twomt_tinker:minevium", {
     groups = {cracky = 1},
     drop = "twomt_tinker:minevium"
 })
+
+minetest.register_ore({
+    ore_type = "blob",
+    ore = "twomt_tinker:minevium",
+    wherein         = {"default:stone"},
+    clust_scarcity  = 16 * 16 * 16,
+    clust_size      = 5,
+    y_min           = -31000,
+    y_max           = 31000,
+    noise_threshold = 0.0,
+    noise_params    = {
+        offset = 0.5,
+        scale = 0.2,
+        spread = {x = 5, y = 5, z = 5},
+        seed = 2019,
+        octaves = 1,
+        persist = 0.0
+    },
+})
