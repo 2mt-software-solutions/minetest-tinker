8633d329172785498527288ee17c2f65ae6bf6b7
2mt-schneider <oliver.schneider@2mt.info>
2019-10-25 20:25:29 +0200

    Make recipes use ingot instead of fabolight blocks

--- src/crafts.lua
+++ src/crafts.lua
@@ -2,7 +2,7 @@ minetest.register_craft({
     type = "shapeless",
     output = "twomt_tinker:fabolight_ice",
     recipe = {
-        "twomt_tinker:fabolight",
+        "twomt_tinker:fabolight_ingot",
         "default:snow"
     }
 })
